159ee77f6095ce38907b162adac812dea5b3f435
Jensen Lee <jensenlee012technology@gmail.com>
2024-11-25 09:31:37 -0800

    Iteration 16.1
    Added controller support to restart button

---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(RestartGame);
+    }
+    void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Shoot"))
+        {
+            RestartGame();
+        }
+    }
+}
